e70e1f87fb290ef8dcdc9993884d700d5898470a
Ruben <rcardenas7613@gmail.com>
2025-11-25 01:31:21 -0600

    Added Like, Delete, Preview

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,10 @@
 const initialCards = [
+
+    {
+        name: "Golden Gate Bridge",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/7-photo-by-griffin-wooldridge-from-pexels.jpg"
+    },
+
     {
         name: "Val Thorens",
         link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg"
@@ -44,6 +50,11 @@ const newPostFormEl = newPostModal.querySelector(".modal__form");
 const newPostImageInput = newPostModal.querySelector("#card-image-input");
 const newPostDescriptionInput = newPostModal.querySelector("#card-description-input")
 
+const previewModal = document.querySelector("#preview-modal");
+const previewModalCloseBtn = previewModal.querySelector(".modal__close-btn_type_preview");
+const previewImageEl = previewModal.querySelector(".modal__image");
+const previewNameEl = previewModal.querySelector(".modal__caption");
+
 const profileNameEl = document.querySelector(".profile__name");
 const profileDescriptionEl = document.querySelector(".profile__description");
 
@@ -61,6 +72,24 @@ function getCardElement(data) {
     cardImageEl.src = data.link;
     cardImageEl.alt = data.name;
     cardTitleEl.textContent = data.name;
+
+    const cardLikeBtnEl = cardElement.querySelector(".card__like-btn");
+    cardLikeBtnEl.addEventListener("click", () => {
+        cardLikeBtnEl.classList.toggle("card__like-btn_active");
+    });
+
+    const cardDeleteBtnEl = cardElement.querySelector(".card__delete-btn");
+    cardDeleteBtnEl.addEventListener("click", () => {
+        cardElement.remove();
+        cardElement = null;
+    });
+
+    cardImageEl.addEventListener("click", () => {
+        previewImageEl.src = data.link;
+        previewImageEl.alt = data.name;
+        previewNameEl.textContent = data.name;
+        openModal(previewModal);
+    });
 
     return cardElement; 
 }
@@ -90,6 +119,8 @@ newPostBtn.addEventListener("click", function () {
 newPostCloseBtn.addEventListener("click", function () {
     closeModal(newPostModal);
 });
+
+previewModalCloseBtn.addEventListener("click", () => closeModal(previewModal));
 
 function handleProfileFormSubmit(evt) {
     evt.preventDefault();
@@ -122,3 +153,4 @@ initialCards.forEach(function (card) {
     const cardElement = getCardElement(card);
     cardList.append(cardElement);
 });
+
